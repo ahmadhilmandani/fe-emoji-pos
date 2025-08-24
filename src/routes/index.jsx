1e53182951dd73080d4afb9fcee8a75b08e7036d
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-08-25 05:33:20 +0700

    init

---
+++ src/routes/index.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-red-200 text-2xl">Hello World</div>,
+  },
+]);
