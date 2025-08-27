9f79a6a45db08d95368fc5e2b363b4e881e0c58c
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-08-28 05:44:02 +0700

    add: react toastify package

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './routes/index.jsx'
 import { RouterProvider } from 'react-router'
+import { Bounce, ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce}
+    />
     <RouterProvider router={router} />
   </StrictMode>
 )
