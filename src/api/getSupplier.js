904b5af277e5b062d9c4f7787bdb5a5f2f4d1898
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-09-20 06:27:59 +0700

    update: get supplier with header auth

--- src/api/getSupplier.js
+++ src/api/getSupplier.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const getSupplier = async () => {
   const token = localStorage.getItem('token')
-  
+
   return axios.get(
     `${import.meta.env.VITE_DEV_API_BASE_URL}/supplier`, {
     headers: {
