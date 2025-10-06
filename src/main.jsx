299cf6715ad7e29be497a6c5d0d9aa15926af52a
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-10-06 16:55:25 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import store from "../src/redux/store.js"
 import { Provider } from 'react-redux'
 import 'react-html5-camera-photo/build/css/index.css';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
